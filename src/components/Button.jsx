4b619b2d3393ae38d6141f80a38b4c4c61e6317f
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-07-20 13:32:03 +0200

    Add buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => <div>{name}</div>;
+const Button = ({ name }) => <button type="button">{name}</button>;
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
