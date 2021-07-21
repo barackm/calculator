12e2f7144c3243c09631dcaa1baa83e75afa1a6a
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-07-21 13:40:10 +0200

    Finish the calculation logic in the calculate module

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,6 +1,71 @@
+import operate from './operate';
+
 const calculate = (data, buttonName) => {
-  const { total, next, operation } = data;
-  console.log(total, next, operation, buttonName);
+  let { total, next, operation } = data;
+  const modifiers = ['AC', '+/-', '%', '.', '='];
+  const operators = ['+', '-', 'X', '÷'];
+  if (modifiers.includes(buttonName)) {
+    switch (buttonName) {
+      case 'AC':
+        total = null;
+        next = null;
+        operation = null;
+        break;
+      case '+/-':
+        if (next === total) {
+          total *= -1;
+          next = total.toString();
+        } else if (!operation) {
+          total *= -1;
+          next = total.toString();
+        }
+        break;
+      case '=':
+        if (next && operation) {
+          if (next.split(`${operation}`)[1]) {
+            const splitted = next.split(`${operation}`);
+            total = operate(splitted[0], splitted[1], operation);
+            operation = null;
+          }
+        }
+        break;
+      case '.':
+        if (next && !operation) {
+          next += '.';
+        } else if (next && operation) {
+          const splitted = next.split(`${operation}`);
+          if (splitted[1]) {
+            next += '.';
+          }
+        }
+        break;
+      default:
+        break;
+    }
+  } else if (operators.includes(buttonName)) {
+    if (!operation) {
+      operation = buttonName;
+      next += operation;
+    } else {
+      const splitted = next.split(`${operation}`);
+      if (!splitted[1]) {
+        operation = buttonName;
+        next = splitted[0] + operation;
+      } else {
+        total = operate(splitted[0], splitted[1], operation);
+        operation = buttonName;
+        next = total;
+      }
+    }
+  } else {
+    const button = buttonName;
+    if (operation) {
+      next += button;
+    } else {
+      next += button;
+    }
+  }
+  return { total, next, operation };
 };
 
 export default calculate;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,24 +1,29 @@
 import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operator) => {
+  const first = numberOne !== null ? Big(numberOne) : 0;
+  const second = numberTwo !== null ? Big(numberTwo) : 0;
   let result = 0;
   switch (operator) {
     case 'X':
-      result = Big(numberOne * numberTwo);
+      result = first.times(second);
       break;
     case '+':
-      result = Big(numberOne + numberTwo);
+      result = first.plus(second);
       break;
     case '-':
-      result = Big(numberOne - numberTwo);
+      result = first.minus(second);
       break;
     case '÷':
-      result = Big(numberOne / numberTwo);
+      result = first.div(second);
+      break;
+    case '%':
+      result = first.times(0.1);
       break;
     default:
       break;
   }
-  return result;
+  return result.toString();
 };
 
 export default operate;
