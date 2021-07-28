c3b6a5fc51791e6508dece7c96686b861e098b0a
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-07-28 13:57:11 +0200

    Dine testing the operate module

---
+++ src/tests/operate.spec.js
@@ -0,0 +1,28 @@
+import operate from '../logic/operate';
+
+describe('Operate', () => {
+  it('Should give the sum of two given numbers is the operator is +', () => {
+    const result = operate(2, 3, '+');
+    expect(result).toBe('5');
+  });
+
+  it('Should give the difference of two given numbers is the operator is -', () => {
+    const result = operate(2, 3, '-');
+    expect(result).toBe('-1');
+  });
+
+  it('Should give the product of two given numbers is the operator is x', () => {
+    const result = operate(2, 3, 'x');
+    expect(result).toBe('6');
+  });
+
+  it('Should give the percentage of the first number is the operator is %', () => {
+    const result = operate(2, 3, '%');
+    expect(result).toBe('0.2');
+  });
+
+  it('Should give the rest of the division is the operator is ÷', () => {
+    const result = operate(10, 2, '÷');
+    expect(result).toBe('5');
+  });
+});
