b4fe28526370d109c4e6468e9bb28bd42ff55cf2
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-07-29 10:32:19 +0200

    Add tests with react-testing-library to interact with the DOM

--- src/tests/calculator.spec.js
+++ src/tests/calculator.spec.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import Calculator from '../components/Calculator';
 
 describe('Calculator Component', () => {
+  beforeEach(() => {
+    render(<Calculator />);
+  });
   it('Should match spanshot', () => {
     const tree = renderer.create(<Calculator />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  it('should give the sum of two clicked numbers with the + button', async () => {
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('+'));
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('='));
+    expect(screen.getByTitle('result')).toHaveTextContent('10');
+  });
+  it('should give the product of two clicked numbers with the x button', async () => {
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('x'));
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('='));
+    expect(screen.getByTitle('result')).toHaveTextContent('25');
   });
 });
